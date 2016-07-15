f9c01e7641b54f398108e1d9c6147b3730422e49
paolodragone <paolo.dragone@unitn.it>
2016-07-15 17:58:42 +0200

    fixed problem with gitingore

--- build/lib/pymzn/_mzn/__init__.py
+++
@@ -1,9 +0,0 @@
-
-__all__ = ['minizinc', 'mzn2fzn', 'solns2out', 'MiniZincUnsatisfiableError',
-           'MiniZincUnknownError', 'MiniZincUnboundedError', 'MiniZincModel',
-           'gecode', 'optimatsat', 'solve']
-
-from ._minizinc import *
-from ._model import *
-from ._solvers import *
-
